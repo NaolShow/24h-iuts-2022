b96c52fd3e7537a4fdefbe1b60d88bd5e4c76e53
Romcharted <romain.sonnet2003@gmail.com>
2022-04-02 13:14:14 +0200

    Create Deck.cs

---
+++ Chardonnay1erCru/Deck.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chardonnay1erCru
+{
+    public class Deck
+    {
+        public List<Card> deck;
+
+        public Deck()
+        {
+            deck = new List<Card>();
+        }
+
+        public void Refresh(string msg)
+        {
+            string[] splitted = msg.Split('|');
+
+            foreach (string split in splitted.Skip(1))
+            {
+                deck.Add(Card.GetCard(split));
+            }
+        }
+    }
+}
