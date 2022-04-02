046fc5a5183343e28097d6b87b13041d4eabde0f
NaolShow <naolyt@gmail.com>
2022-04-02 11:17:52 +0200

    feat(base): ajout de la base du projet

---
+++ Chardonnay1erCru/Program.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace Chardonnay1erCru {
+    internal class Program {
+        static void Main(string[] args) {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
