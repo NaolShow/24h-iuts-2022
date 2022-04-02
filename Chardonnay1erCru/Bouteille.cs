6478e094716b2294eed5d3aa020ff19e9500cfde
Victor CADOUX <vc432120@iut21.u-bourgogne.fr>
2022-04-02 12:09:44 +0200

    Ajout class BOUTEILLE

---
+++ Chardonnay1erCru/Bouteille.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chardonnay1erCru
+{
+    public class Bouteille
+    {
+        private string nom;
+
+        private int quantite;
+
+
+
+        public string Nom 
+        {
+            get { return nom; }
+            set { nom = value; }
+        }
+
+        public int Quantite
+        {
+            get { return quantite; }
+            set { quantite = value; }
+        }
+
+
+
+    }
+}
